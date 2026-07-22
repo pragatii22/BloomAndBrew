325008440d65714144e323ce891ca4350365082e
Pragati Gaire <pragatishahi022@gmail.com>
2026-07-22 12:09:26 +0545

    Remove obsolete components and pages

--- frontend/src/components/Loader.jsx
+++
@@ -1,9 +0,0 @@
-const Loader = () => {
-  return (
-    <div className="flex items-center justify-center py-10">
-      <div className="h-10 w-10 animate-spin rounded-full border-4 border-pink-200 border-t-pink-500"></div>
-    </div>
-  );
-};
-
-export default Loader;
